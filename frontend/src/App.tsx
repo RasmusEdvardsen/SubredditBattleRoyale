72dcf30cb0df5ad6dabc11be78f8d48f84efe19f
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-13 14:37:10 +0100

    Ideas for listening to events. Needs more work.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
+import { startListenTokensPurchased } from './contract';
 import './App.css';
 
 function App() {
+  const [tokenBalances, setTokenBalances] = useState<{ [subreddit: string]: number }>({});
+  startListenTokensPurchased(
+    (data) => {
+      setTokenBalances((prevBalances) => ({
+        ...prevBalances,
+        [data.returnValues.subreddit as string]: (prevBalances[data.returnValues.subreddit as string] || 0) + Number(data.returnValues.amount),
+      }));
+      console.log(`\nTokens purchased!\nBuyer: ${data.returnValues.buyer}\nSubreddit: ${data.returnValues.subreddit}\nAmount: ${data.returnValues.amount}`);
+      console.log(`Unique id: block# ${data.blockHash}, txn# ${data.transactionHash}, logIndex ${data.logIndex}`)
+    },
+    (err: Error) => {
+      console.log("\nError occurred: ", err);
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <p>
+        Subreddit Battle Royale
+      </p>
+      <div>
+        <h2>Token Balances</h2>
+        <ul>
+          {Object.entries(tokenBalances).map(([subreddit, balance]) => (
+            <li key={subreddit}>
+              <p>{subreddit}: {Number(balance)}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </body>
   );
 }
 
--- frontend/src/contract.ts
+++ frontend/src/contract.ts
@@ -1,43 +1,46 @@
-// consider uninstall web3, install web3-eth
-import { Web3 } from "web3";
-import * as fs from 'fs';
+// todo: consider uninstall web3, install web3-eth
+// todo: either keep state up until block N off-chain and fetch events from block N+1, or just fetch all events.
+// todo: Events: Tokens purchased, Tokens burned, Season won
+// todo: State (Update if changed by events somehow): Current season, voidTokens, BURN_MULTIPLIER, TOKEN_PRICE
+import { Contract, Web3, EventLog } from "web3";
+import ABI from './abi';
 
-const INFURA_API_KEY = "INFURA_API_KEY";
+const INFURA_API_KEY = "2c3e5f5cd3fd4fdf8e15562f1ddb3e83";
 const CONTRACT_ADDRESS = "0xea8831bcb719914ab97131f48d9b2dc737dbd25a";
+const PROVIDER = new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
+const WEB3 = new Web3(PROVIDER);
 
-const jsonContractFilePath = "..\\ignition\\deployments\\mainnet-deployment\\artifacts\\SubredditBattleRoyaleModule#SubredditBattleRoyale.json";
+let _CONTRACT: Contract<any>;
+async function CONTRACT() {
+    if (_CONTRACT == null) {
+        _CONTRACT = await new WEB3.eth.Contract(ABI.abi, CONTRACT_ADDRESS);
+    }
 
-// todo: stuff I need:
-// Events:
-// 1. Tokens purchased
-// 2. Tokens burned
-// 3. Season won
-// State:
-// 1. Current season (Update if changed)
-// 2. voidTokens (Update if changed)
-// 3. BURN_MULTIPLIER (Update if changed)
-// 4. TOKEN_PRICE
-async function main() {
-    const provider = new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
-    const web3 = new Web3(provider);
-
-    const jsonContractFile = await fs.promises.readFile(jsonContractFilePath, 'utf8');
-    const jsonContract = JSON.parse(jsonContractFile);
-
-    const contract = new web3.eth.Contract(jsonContract.abi, CONTRACT_ADDRESS);
-
-    // todo: either I keep state up until block N off-chain and fetch events from block N+1, or I just fetch all events.
-    const event = await contract.events.TokensPurchased({fromBlock: 0});
-
-    event.on('data', (data) => {
-        console.log(`\nTokens purchased!\nBuyer: ${data.returnValues.buyer}\nSubreddit: ${data.returnValues.subreddit}\nAmount: ${data.returnValues.amount}`);
-        console.log(`Unique id: block# ${data.blockHash}, txn# ${data.transactionHash}, logIndex ${data.logIndex}`)
-    });
-    event.on('error', (err: Error) => {
-        console.log("\nError occurred: ", err);
-    });
+    return _CONTRACT;
 }
 
-main()
-    .then()
-    .catch(console.error);
+async function startListenTokensPurchased(onData: (data: EventLog) => void, onError: (err: Error) => void) {
+    const contract = await CONTRACT();
+
+    const latestBlock = await WEB3.eth.getBlock('latest');
+
+    const pastEventsUnfiltered: (string | EventLog)[] = await contract.getPastEvents('TokensPurchased', {
+        fromBlock: 0,
+        toBlock: latestBlock.number
+    });
+    
+    function isEventLog(item: (string | EventLog)): item is EventLog {
+        return typeof item !== 'string';
+    }
+
+    const pastEvents = pastEventsUnfiltered.filter(isEventLog);
+
+    console.log(pastEvents);
+
+    const tokensPurchasedEvent = contract.events.TokensPurchased({ fromBlock: 'latest' });
+
+    tokensPurchasedEvent.on('data', onData);
+    tokensPurchasedEvent.on('error', onError);
+}
+
+export { startListenTokensPurchased };
