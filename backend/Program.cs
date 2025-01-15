ad5da59ba9c5fe842ac41a372fcddac903a5603f
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-15 16:48:05 +0100

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -20,5 +20,4 @@ app.Run();
 // todo: create database that stores all types of events, INCLUDING BLOCK NUMBER
 // todo: when new request, only request events from latest block number in database
 // todo: probably rate limit not the API, but calls to blockchain somehow, when API is called (stale data is fine to some extent)
-// todo: rebase and remove url with API key to alchemy
 // todo: allow query from-block=blockNumber&to-block=blockNumber
