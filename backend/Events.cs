7e1160b185e2b3fb505f4bf9da7941e527a0bfda
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-18 20:02:20 +0100

    Implement persist to db

--- backend/Events.cs
+++ backend/Events.cs
@@ -1,6 +1,10 @@
-﻿using System.Numerics;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using Dapper;
+using Microsoft.Data.Sqlite;
 using Nethereum.ABI.FunctionEncoding.Attributes;
-using Nethereum.Hex.HexTypes;
+using Nethereum.Contracts;
+using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 
 namespace Backend
@@ -11,78 +15,122 @@ namespace Backend
         
         public async Task<AllEvents> GetAllEvents()
         {
-            var tokensPurchasedEvents = await GetEventLogs<TokensPurchasedEvent>(ContractAddress);
-            var tokensBurnedEventEvents = await GetEventLogs<TokensBurnedEvent>(ContractAddress);
-            var seasonWonEventEvents = await GetEventLogs<SeasonWonEvent>(ContractAddress);
+            using var connection = new SqliteConnection("Data Source=hello.db");
+            
+            var tokensPurchased = await connection.QueryAsync<TokensPurchased>("SELECT * FROM TokensPurchased");
+            
+            var fromBlock = tokensPurchased.Any() ? tokensPurchased.Max(e => e.BlockNumber) + 1 : 0;
+            
+            var tokensPurchasedEventsBlockChain = await GetEventLogs<TokensPurchasedEvent>(ContractAddress, fromBlock);
+            var tokensPurchasedEvents = tokensPurchasedEventsBlockChain.Select(TokensPurchased.FromBlockChain);
+            
+            foreach (var eventItem in tokensPurchasedEvents)
+            {
+                connection.Execute(@"
+                    INSERT INTO TokensPurchased (Id, Buyer, Subreddit, Tokens, BlockHash, TransactionHash, LogIndex, BlockNumber)
+                    VALUES (@Id, @Buyer, @Subreddit, @Tokens, @BlockHash, @TransactionHash, @LogIndex, @BlockNumber)", eventItem);
+            }
+            
+            tokensPurchasedEvents = await connection.QueryAsync<TokensPurchased>("SELECT * FROM TokensPurchased");
 
-            return new AllEvents(tokensPurchasedEvents, tokensBurnedEventEvents, seasonWonEventEvents);
+            return new AllEvents(tokensPurchasedEvents, [], []);
         }
 
-        private async Task<IEnumerable<TEventDTO>> GetEventLogs<TEventDTO>(string contractAddress) where TEventDTO : IEventDTO, IUniqueLog, new()
+        private async Task<IEnumerable<EventLog<TEventDTO>>> GetEventLogs<TEventDTO>(string contractAddress, ulong fromBlock) where TEventDTO : IEventDTO, new()
         {
             var eventHandler = web3.Eth.GetEvent<TEventDTO>(contractAddress);
+            
             var filter = eventHandler.CreateFilterInput();
-            var events = await eventHandler.GetAllChangesAsync(filter);
-            var mappedEvents = events.Select(s =>
-            {
-                var mappedEvent = s.Event;
-                mappedEvent.BlockHash = s.Log.BlockHash;
-                mappedEvent.TransactionHash = s.Log.TransactionHash;
-                mappedEvent.LogIndex = s.Log.LogIndex;
-                mappedEvent.BlockNumber = s.Log.BlockNumber;
-                return mappedEvent;
-            });
-            return mappedEvents;
+            filter.FromBlock = new BlockParameter(fromBlock);
+            
+            return await eventHandler.GetAllChangesAsync(filter);
         }
     }
 
-    public interface IUniqueLog
+    public interface ILog
     {
-        string? BlockHash { get; set; }
-        string? TransactionHash { get; set; }
-        HexBigInteger? LogIndex { get; set; }
-        HexBigInteger? BlockNumber { get; set; }
+        string BlockHash { get; set; }
+        string TransactionHash { get; set; }
+        ulong LogIndex { get; set; }
+        ulong BlockNumber { get; set; }
     }
 
     public record struct AllEvents(
-        IEnumerable<TokensPurchasedEvent> TokensPurchasedEvents,
-        IEnumerable<TokensBurnedEvent> TokensBurnedEvents,
-        IEnumerable<SeasonWonEvent> SeasonWonEvents);
+        IEnumerable<TokensPurchased> TokensPurchased,
+        IEnumerable<TokensBurned> TokensBurned,
+        IEnumerable<SeasonWon> SeasonWon);
 
 
     [Event("TokensPurchased")]
-    public record TokensPurchasedEvent : IEventDTO, IUniqueLog
+    public record TokensPurchasedEvent : IEventDTO
     {
         [Parameter("address", "buyer", 1, true)] public string? Buyer { get; set; }
         [Parameter("string", "subreddit", 2, false)] public string? Subreddit { get; set; }
         [Parameter("uint256", "amount", 3, false)] public BigInteger Tokens { get; set; }
-        public string? BlockHash { get; set; }
-        public string? TransactionHash { get; set; }
-        public HexBigInteger? LogIndex { get; set; }
-        public HexBigInteger? BlockNumber { get; set; }
     }
 
     [Event("TokensBurned")]
-    public record TokensBurnedEvent : IEventDTO, IUniqueLog
+    public record TokensBurnedEvent : IEventDTO
     {
         [Parameter("address", "buyer", 1, true)] public string? Buyer { get; set; }
         [Parameter("string", "subreddit", 2, false)] public string? Subreddit { get; set; }
         [Parameter("uint256", "amount", 3, false)] public BigInteger Tokens { get; set; }
-        public string? BlockHash { get; set; }
-        public string? TransactionHash { get; set; }
-        public HexBigInteger? LogIndex { get; set; }
-        public HexBigInteger? BlockNumber { get; set; }
     }
 
     [Event("SeasonWon")]
-    public record SeasonWonEvent : IEventDTO, IUniqueLog
+    public record SeasonWonEvent : IEventDTO
     {
         [Parameter("string", "subreddit", 1, true)] public string? Subreddit { get; set; }
         [Parameter("uint256", "tokens", 2, false)] public string? Tokens { get; set; }
         [Parameter("uint256", "season", 3, false)] public BigInteger Season { get; set; }
-        public string? BlockHash { get; set; }
-        public string? TransactionHash { get; set; }
-        public HexBigInteger? LogIndex { get; set; }
-        public HexBigInteger? BlockNumber { get; set; }
+    }
+
+    public record TokensPurchased : ILog
+    {
+        public required string Id { get; set; }
+        public required string Buyer { get; set; }
+        public required string Subreddit { get; set; }
+        public ulong Tokens { get; set; }
+        public required string BlockHash { get; set; }
+        public required string TransactionHash { get; set; }
+        public ulong LogIndex { get; set; }
+        public ulong BlockNumber { get; set; }
+
+        public static TokensPurchased FromBlockChain(EventLog<TokensPurchasedEvent> eventItem)
+        {
+            return new()
+            {
+                Id = $"{eventItem.Log.BlockHash}_{eventItem.Log.TransactionHash}_{eventItem.Log.LogIndex}",
+                Buyer = eventItem.Event.Buyer!,
+                Subreddit = eventItem.Event.Subreddit!,
+                Tokens = (ulong)eventItem.Event.Tokens,
+                BlockHash = eventItem.Log.BlockHash,
+                TransactionHash = eventItem.Log.TransactionHash,
+                LogIndex = (ulong)eventItem.Log.LogIndex.Value,
+                BlockNumber = (ulong)eventItem.Log.BlockNumber.Value
+            };
+        }
+    }
+
+    public record TokensBurned : ILog
+    {
+        public required string Buyer { get; set; }
+        public required string Subreddit { get; set; }
+        public ulong Tokens { get; set; }
+        public required string BlockHash { get; set; }
+        public required string TransactionHash { get; set; }
+        public ulong LogIndex { get; set; }
+        public ulong BlockNumber { get; set; }
+    }
+
+    public record SeasonWon : ILog
+    {
+        public required string Subreddit { get; set; }
+        public required string Tokens { get; set; }
+        public ulong Season { get; set; }
+        public required string BlockHash { get; set; }
+        public required string TransactionHash { get; set; }
+        public ulong LogIndex { get; set; }
+        public ulong BlockNumber { get; set; }
     }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -12,12 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.Configure<JsonOptions>(options =>
-{
-    options.SerializerOptions.Converters.Add(new BigIntegerConverter());
-    options.SerializerOptions.Converters.Add(new HexBigIntegerConverter());
-});
-
 var app = builder.Build();
 
 app.UseHttpsRedirection();
@@ -34,71 +28,48 @@ static void SetupTeardownDatabase()
 
     connection.Open();
     
-    var createTokensPurchasedEventTable = @"
-        CREATE TABLE IF NOT EXISTS TokensPurchasedEvent (
-            buyer TEXT,
-            subreddit TEXT,
-            tokens INTEGER,
-            blockHash TEXT,
-            transactionHash TEXT,
-            logIndex INTEGER,
-            blockNumber INTEGER,
-            PRIMARY KEY (blockHash, transactionHash, logIndex)
+    var createTokensPurchasedTable = @"
+        CREATE TABLE IF NOT EXISTS TokensPurchased (
+            Id TEXT PRIMARY KEY,
+            Buyer TEXT NOT NULL,
+            Subreddit TEXT NOT NULL,
+            Tokens INTEGER NOT NULL,
+            BlockHash TEXT NOT NULL,
+            TransactionHash TEXT NOT NULL,
+            LogIndex INTEGER NOT NULL,
+            BlockNumber INTEGER NOT NULL
         )";
-    connection.Execute(createTokensPurchasedEventTable);
+    connection.Execute(createTokensPurchasedTable);
 
-    var createTokensBurnedEventTable = @"
-        CREATE TABLE IF NOT EXISTS TokensBurnedEvent (
-            buyer TEXT,
-            subreddit TEXT,
-            tokens INTEGER,
-            blockHash TEXT,
-            transactionHash TEXT,
-            logIndex INTEGER,
-            blockNumber INTEGER,
-            PRIMARY KEY (blockHash, transactionHash, logIndex)
+    var createTokensBurnedTable = @"
+        CREATE TABLE IF NOT EXISTS TokensBurned (
+            Buyer TEXT NOT NULL,
+            Subreddit TEXT NOT NULL,
+            Tokens INTEGER NOT NULL,
+            BlockHash TEXT NOT NULL,
+            TransactionHash TEXT NOT NULL,
+            LogIndex INTEGER NOT NULL,
+            BlockNumber INTEGER NOT NULL,
+            PRIMARY KEY (BlockHash, TransactionHash, LogIndex)
         )";
-    connection.Execute(createTokensBurnedEventTable);
+    connection.Execute(createTokensBurnedTable);
 
-    var createSeasonWonEventTable = @"
-        CREATE TABLE IF NOT EXISTS SeasonWonEvent (
-            subreddit TEXT,
-            tokens INTEGER,
-            Season INTEGER,
-            blockHash TEXT,
-            transactionHash TEXT,
-            logIndex INTEGER,
-            blockNumber INTEGER,
-            PRIMARY KEY (blockHash, transactionHash, logIndex)
+    var createSeasonWonTable = @"
+        CREATE TABLE IF NOT EXISTS SeasonWon (
+            Subreddit TEXT NOT NULL,
+            Tokens INTEGER NOT NULL,
+            Season INTEGER NOT NULL,
+            BlockHash TEXT NOT NULL,
+            TransactionHash TEXT NOT NULL,
+            LogIndex INTEGER NOT NULL,
+            BlockNumber INTEGER NOT NULL,
+            PRIMARY KEY (BlockHash, TransactionHash, LogIndex)
         )";
-    connection.Execute(createSeasonWonEventTable);
-
-    // Clean up
-    connection.Dispose();
-    SqliteConnection.ClearAllPools();
-    File.Delete("hello.db");
+    connection.Execute(createSeasonWonTable);
 }
 
-// todo: when new request, only request events from latest block number in database
+// todo: implement for Events.cs tokensBurned and seasonWon
 // todo: probably rate limit not the API, but calls to blockchain somehow, when API is called (stale data is fine to some extent)
 // todo: allow query from-block=blockNumber&to-block=blockNumber
 // todo: move Events.ContractAddress in to appsettings.json
 // todo: hide Events.AlchemyApiKey apiKey somehow
-
-public class BigIntegerConverter : JsonConverter<BigInteger>
-{
-    public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-        throw new NotImplementedException();
-
-    public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options) =>
-        writer.WriteRawValue(value.ToString(NumberFormatInfo.InvariantInfo), false);
-}
-
-public class HexBigIntegerConverter : JsonConverter<HexBigInteger>
-{
-    public override HexBigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-        throw new NotImplementedException();
-
-    public override void Write(Utf8JsonWriter writer, HexBigInteger value, JsonSerializerOptions options) =>
-        writer.WriteRawValue(value.Value.ToString(NumberFormatInfo.InvariantInfo), false);
-}
