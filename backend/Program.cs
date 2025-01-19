8e1ef4bc4ef29b5c551b4f55c1dff785d3630cf9
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-20 00:05:25 +0100

    Remove todo

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,7 @@
 using Backend;
 
+await DatabaseSetup.SetupTeardownDatabase();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -8,13 +10,10 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-await DatabaseSetup.SetupTeardownDatabase();
-
 app.MapGet("/events", async () => await new Events().GetAllEvents());
 
 app.Run();
 
-// todo: probably rate limit not the API, but calls to blockchain somehow, when API is called (stale data is fine to some extent)
 // todo: allow query from-block=blockNumber&to-block=blockNumber
 // todo: move Events.ContractAddress in to appsettings.json
 // todo: hide Events.AlchemyApiKey apiKey somehow
