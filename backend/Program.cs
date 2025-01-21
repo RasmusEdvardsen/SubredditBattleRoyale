a18a4d1543a3dfb5cb8a2eb81b53c9937c51972c
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-21 21:48:29 +0100

    Add new backend

---
+++ backend/Program.cs
@@ -0,0 +1,16 @@
+using Backend.Functionality;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+await DatabaseSetup.SetupTeardownDatabase();
+
+builder.Build().Run();
