21972a4d9462d278cac87cb2009143a64983b854
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-27 21:11:30 +0100

    Initial stuff in frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,22 @@ import './App.css';
 
 import Wallet from './components/Wallet';
 
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 function App() {
+  const [backendData, setBackendData] = useState(null);
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_BACKEND_URI)
+      .then(response => setBackendData(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
-      <p>App!</p>
+      <p>App! {backendData}</p>
       <Wallet />
     </div>
   );
