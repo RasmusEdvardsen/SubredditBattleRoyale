0c5336acd01f9b736e9464eccaa9208ff28e903d
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-01-27 21:41:34 +0100

    Display backend data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,8 +17,22 @@ function App() {
 
   return (
     <div className="App">
-      <p>App! {backendData}</p>
+      <h1>Subreddit Battle Royale</h1>
+
       <Wallet />
+
+      <h3>Void Token Count</h3>
+      <p>{backendData?.voidTokenCount == null ? "" : JSON.stringify(backendData.voidTokenCount)}</p>
+
+      <h3>Tokens Purchased</h3>
+      <ul>{backendData?.tokensPurchased.map((tokensPurchased, index) => <li key={index}>{tokensPurchased.id}</li>)}</ul>
+
+      <h3>Tokens Burned</h3>
+      <ul>{backendData?.tokensBurned.map(token => <li>{JSON.stringify(token)}</li>)}</ul>
+
+      <h3>Season Won</h3>
+      <ul>{backendData?.seasonWon.map(season => <li>{JSON.stringify(season)}</li>)}</ul>
+
     </div>
   );
 }
