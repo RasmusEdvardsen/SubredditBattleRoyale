949c583ec8b4c55469f1623b40f70f617c374ab0
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-02-13 13:56:39 +0100

    Add styling and set width on text containers in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -51,74 +51,49 @@ watch(backendData, (newData) => {
 </script>
 
 <template>
+  <!-- todo: do better v-ifs (check further in on each props) -->
 
-  <body>
-    <div className="App">
-      <!-- todo: do better v-ifs (check further in on each props) -->
+  <BubbleCloud :rawData="aggregatedData" />
 
-      <BubbleCloud :rawData="aggregatedData" />
+  <div id="intro-and-title">
+    <h1>Subreddit Battle Royale</h1>
+    <p>
+      Welcome to the Subreddit Battle Royale!
+      In this game, subreddits compete against each other by purchasing and burning tokens.
+      The goal is to accumulate the more tokens than the void token count, thereby winning the season.
+      <br>We refresh blockchain data from the backend every 15 seconds to keep the competition up-to-date.
+      <br>The cost of a token is 0.0001 Ether. We recommend buying at least 10 tokens per transaction to save on gas
+      fees.
+      <br>Good luck and may the best subreddit win!
+    </p>
+  </div>
 
-      <h1>Subreddit Battle Royale</h1>
-      <p>
-        Welcome to the Subreddit Battle Royale!
-        In this game, subreddits compete against each other by purchasing and burning tokens.
-        The goal is to accumulate the more tokens than the void token count, thereby winning the season.
-        <br>We refresh blockchain data from the backend every 15 seconds to keep the competition up-to-date.
-        <br>The cost of a token is 0.0001 Ether. We recommend buying at least 10 tokens per transaction to save on gas fees.
-        <br>Good luck and may the best subreddit win!
-      </p>
+  <Wallet />
 
-      <Wallet />
+  <!-- todo: move in to component as well -->
+  <div v-if="backendData" id="void-token-count">
+    <h3>Void Token Count: {{ backendData.voidTokenCount.balance }}</h3>
+  </div>
 
-      <!-- todo: move in to component as well -->
-      <div v-if="backendData">
-        <h3>Void Token Count</h3>
-        <p>{{ backendData.voidTokenCount.balance }}</p>
-      </div>
+  <!-- todo: move in to component as well -->
+  <div v-if="backendData && backendData.seasonWon.length > 0">
+    <h3>Seasons Won</h3>
+    <li v-for="seasonWon in backendData.seasonWon" :key="seasonWon.id">
+      Season {{ seasonWon.season }} was won by {{ seasonWon.subreddit }}.
+    </li>
+  </div>
+  <div v-else>
+    <h3>No seasons won yet.</h3>
+  </div>
 
-      <!-- todo: move in to component as well -->
-      <div v-if="backendData && backendData.seasonWon.length > 0">
-        <h3>Seasons Won</h3>
-        <li v-for="seasonWon in backendData.seasonWon" :key="seasonWon.id">
-          Season {{ seasonWon.season }} was won by {{ seasonWon.subreddit }}.
-        </li>
-      </div>
-      <div v-else>
-        <h3>No seasons won yet.</h3>
-      </div>
+  <SubredditBalances :aggregatedData="aggregatedData" />
 
-      <SubredditBalances :aggregatedData="aggregatedData" />
-
-      <Events :backendData="backendData" />
-    </div>
-  </body>
+  <Events :backendData="backendData"/>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#intro-and-title {
+  max-width: 80%;
+  text-align: center;
 }
 </style>
