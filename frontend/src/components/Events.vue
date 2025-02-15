bd5b4d1b0c7c75e456f68a477e092e51329c2c81
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2025-02-15 15:24:16 +0100

    Rearrange table

--- frontend/src/components/Events.vue
+++ frontend/src/components/Events.vue
@@ -14,20 +14,20 @@ const { backendData } = defineProps<{
             <thead>
                 <tr>
                     <th>Event Type</th>
-                    <th>Block No.</th>
-                    <th>Transaction (#)</th>
                     <th>Subreddit</th>
                     <th>Amount</th>
+                    <th>Transaction (#)</th>
+                    <th>Block No.</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="transaction in [...backendData.tokensPurchased, ...backendData.tokensBurned].sort((a, b) => b.blockNumber - a.blockNumber)"
                     :key="transaction.id">
                     <td>{{ transaction.eventType }}</td>
-                    <td>{{ transaction.blockNumber }}</td>
-                    <td>{{ transaction.transactionHash }}</td>
                     <td>{{ transaction.subreddit }}</td>
                     <td>{{ transaction.tokens }}</td>
+                    <td>{{ transaction.transactionHash }}</td>
+                    <td>{{ transaction.blockNumber }}</td>
                 </tr>
             </tbody>
         </table>
